e0f77188e97f241bbb36010d9b3e42cdbb0beae8
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2020-11-02 15:19:27 -0800

    initial commit

---
+++ nerdlets/nr1-testing-intro-nerdlet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class Nr1TestingIntroNerdletNerdlet extends React.Component {
+    render() {
+        return <h1>Hello, nr1-testing-intro-nerdlet Nerdlet!</h1>;
+    }
+}
