145125da3dc157dce4db5888df9bbec4f61f17de
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2020-11-02 19:17:55 -0800

    add an icon

--- nerdlets/nr1-testing-intro-nerdlet/index.js
+++ nerdlets/nr1-testing-intro-nerdlet/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { Icon } from 'nr1';
+
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
 export default class Nr1TestingIntroNerdletNerdlet extends React.Component {
     render() {
-        return <h1>Hello, nr1-testing-intro-nerdlet Nerdlet!</h1>;
+        return (
+          <div>
+            <h1>Hello, nr1-testing-intro-nerdlet Nerdlet!</h1>
+            <Icon type={Icon.TYPE.HARDWARE_AND_SOFTWARE__SOFTWARE__PLUGIN__S_OK} />
+          </div>
+        );
     }
 }
